83e9449d0171ac53a1e7300489d84acb314fb072
Vladimir Solovyov <leilyngale@icloud.com>
2022-08-11 17:03:14 +0300

    Scaffolded the ecommerce project to be built with React

---
+++ src/App.js
@@ -0,0 +1,47 @@
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body">
+          <h2>Hats</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body">
+          <h2>Jackets</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body">
+          <h2>Sneakers</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body">
+          <h2>Womens</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body">
+          <h2>mens</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
