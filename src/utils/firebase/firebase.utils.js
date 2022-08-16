1c62945f7dd4ea1a89ae9a07956a78cd4b34baae
Vladimir Solovyov <leilyngale@icloud.com>
2022-08-16 08:33:56 +0300

    Set up creation of a user inn the Firebase on sign-in

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,6 +3,13 @@ import { getAuth,
         signInWithPopup, 
         signInWithRedirect,
         GoogleAuthProvider } from 'firebase/auth';
+
+import {
+  getFirestore,
+  doc,
+  getDoc, 
+  setDoc
+} from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyC-3Gf4EcSaXiD8HPUeI1uTpcr6Ev4Fvp8",
@@ -22,4 +29,31 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
-  
+
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async(userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+   // console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    // console.log(userSnapshot);
+    // console.log(userSnapshot.exists());
+
+    if(!userSnapshot.exists()){
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt
+        });
+      } catch(error){
+        console.log('error creating user', error.message);
+      }
+    }
+    return userDocRef;
+  }
